adf3e4aec44b043b4155ed5992612b71532e4931
亜門 <heavy.metal.thunder.20120415@gmail.com>
2025-07-11 23:56:27 +0900

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App_step2.jsx' // App.jsx をインポート
+import App from './App_step1.jsx' // App.jsx をインポート
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
